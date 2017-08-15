ded14fff88587fa5be4c0f986fc91667c663bac2
Per-Torben Sørensen <per-torben@home.sorensen.ws>
2017-08-15 12:47:03 +0200

    Initial

---
+++ Tesla.ps1
@@ -0,0 +1,24 @@
+#Get-Tesla commands:
+#climate_state
+#charge_state
+#gui_settings
+#drive_state
+#vehicle_state
+#vehicles
+#Set-Tesla commands:
+#mobile_enabled
+#auto_conditioning_start
+#auto_conditioning_stop
+#door_lock
+#door_unlock
+#sun_roof_control?state=close
+#sun_roof_control?state=comfort
+#sun_roof_control?state=vent
+#charge_stop
+#charge_start
+
+Connect-Tesla
+
+get-tesla -Command vehicles
+
+Get-Tesla -Command charge_state
