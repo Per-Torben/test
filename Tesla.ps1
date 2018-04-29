3abbd4d9da4b483ec6906a4be09c3369a7160f55
Per-Torben Sørensen <per-torben@sorensen.ws>
2018-04-29 20:28:23 +0200

    updater

--- Tesla.ps1
+++ Tesla.ps1
@@ -1,25 +1,4 @@
-#Get-Tesla commands:
-#climate_state
-#charge_state
-#gui_settings
-#drive_state
-#vehicle_state
-#vehicles
-#Set-Tesla commands:
-#mobile_enabled
-#auto_conditioning_start
-#auto_conditioning_stop
-#door_lock
-#door_unlock
-#sun_roof_control?state=close
-#sun_roof_control?state=comfort
-#sun_roof_control?state=vent
-#charge_stop
-#charge_start
-#$teslacred = Get-Credential
-#Connect-Tesla -Credential $teslacred
-#get-tesla -Command vehicles    
-#Get-Tesla -Command charge_state
+
 Import-Module tesla
 $token = Get-TeslaToken -Credential (Get-Credential)
 $vehicle = Get-TeslaVehicle -Token $token
